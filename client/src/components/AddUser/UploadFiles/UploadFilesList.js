1eaac4f36b4fea73dc304f37622dd048a0a80abc
Pavel Vynychenko <pavel.vynych@gmail.com>
2020-08-05 00:03:54 +0300

    clear useless code

--- client/src/components/AddUser/UploadFiles/UploadFilesList.js
+++ client/src/components/AddUser/UploadFiles/UploadFilesList.js
@@ -1,34 +1,13 @@
 import React, { useState } from 'react';
-import { useApolloClient, useMutation, gql } from '@apollo/client';
-
-
-
 
 export const UploadFileList = (props) => {
 
-	const MULTIPLE_UPLOAD_MUTATION = gql`
-	mutation multipleUpload($files: [Upload!]!
-	  ) {
-	  multipleUpload(
-		  files: $files
-	  ) {
-		id
-	  }
-	}
-  `;
-
 	const [filesState, setState] = useState([]);
-	// const [multipleUploadMutation] = useMutation(MULTIPLE_UPLOAD_MUTATION);
-	const apolloClient = useApolloClient();
-
 	const handleAddFiles = ({ target: { validity, files} }) => {
 		setState({
 			'files': files
 		}, props.updateFiles(files));
 	}
-
-
-	
 
 	return (
 		<div>
